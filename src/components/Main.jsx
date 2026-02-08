9a4e3a630a640abcd891f2dd437ac95541c65b6f
Grady Masirika <gradyaganzemasirika@gmail.com>
2026-02-08 15:12:26 +0100

    Arrival section (#19)
    * feat: update App.jsx before pull
    
    * feat: finish feature section
    
    * feat : complete arrival section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from './Hero'
 import FeatureBar from './FeatureBar';
+import ArrivalSection from './ArrivalSection';
+
 
 function Main() {
   return (
     <div>
       <Hero></Hero>
       <FeatureBar></FeatureBar>
+      <ArrivalSection></ArrivalSection>
+
     </div>
   )
 }
