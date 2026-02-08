e140d60877be807a62817bfe4f2049227ed6768f
Grady Masirika <gradyaganzemasirika@gmail.com>
2026-02-08 11:46:27 +0100

    feat: update App.jsx before pull (#17)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Header from './components/Header'
+
+import Main from './components/Main';
+
 function App() {
   return (
     <>
-      <Header />
-    
+      <div>
+        <Header />
+        <Main></Main>
+      </div>
     </>
   )
 }
