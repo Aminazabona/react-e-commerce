c12718fe38ed8be30e0705ec2e53d61fae2bb3ce
Amina Zabona <aminazabona8@gmail.com>
2026-02-10 16:58:16 +0100

    Blog (#35)
    * feat: add footer component
    
    * feat: footer
    
    * feat:complete blog section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Main from './components/Main'
+import BlogSection from './components/Blog'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div>
         <Header />
         <Main></Main>
+        <BlogSection />
         <Footer />
       </div>
     </>
